415ac0cdb2ca591103ffc972dd188d16d27c0d7f
Taewii <p3rfec7@abv.bg>
2019-04-17 19:47:04 +0300

    1-2 solutions

--- 16. Advanced Graph Algorithms Part II - Lab/src/p01_articulation_points/ArticulationPoints.java
+++ 16. Advanced Graph Algorithms Part II - Lab/src/p01_articulation_points/ArticulationPoints.java
@@ -1,10 +1,61 @@
 package p01_articulation_points;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticulationPoints {
 
+    private static List<Integer>[] graph;
+    private static boolean[] visited;
+    private static int[] depths;
+    private static int[] lowpoint;
+    private static Integer[] parent;
+
+    private static List<Integer> articulationPoints;
+
     public static List<Integer> findArticulationPoints(List<Integer>[] targetGraph) {
-        return null;
+        graph = targetGraph;
+        visited = new boolean[graph.length];
+        depths = new int[graph.length];
+        lowpoint = new int[graph.length];
+        parent = new Integer[graph.length];
+        articulationPoints = new ArrayList<>();
+
+        for (int node = 0; node < graph.length; node++) {
+            if (!visited[node]) {
+                findArticulationPoints(node, 1);
+            }
+        }
+
+        return articulationPoints;
+    }
+
+    private static void findArticulationPoints(int node, int depth) {
+        visited[node] = true;
+        depths[node] = depth;
+        lowpoint[node] = depth;
+
+        boolean isArticulationPoint = false;
+        int childrenCount = 0;
+
+        for (Integer child : graph[node]) {
+            if (!visited[child]) {
+                parent[child] = node;
+                findArticulationPoints(child, depth + 1);
+                childrenCount++;
+
+                if (lowpoint[child] >= depths[node]) {
+                    isArticulationPoint = true;
+                }
+
+                lowpoint[node] = Math.min(lowpoint[node], lowpoint[child]);
+            } else if (!child.equals(parent[node])) {
+                lowpoint[node] = Math.min(lowpoint[node], depths[child]);
+            }
+        }
+
+        if ((parent[node] == null && childrenCount > 1) || (parent[node] != null && isArticulationPoint)) {
+             articulationPoints.add(node);
+        }
     }
 }
--- 16. Advanced Graph Algorithms Part II - Lab/src/p02_max_flow/EdmondsKarp.java
+++ 16. Advanced Graph Algorithms Part II - Lab/src/p02_max_flow/EdmondsKarp.java
@@ -1,8 +1,69 @@
 package p02_max_flow;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class EdmondsKarp {
 
-    public static int findMaxFlow(int[][]targetGraph) {
-        return 0;
+    private static int[][] graph;
+    private static Integer[] parent;
+
+    public static int findMaxFlow(int[][] targetGraph) {
+        graph = targetGraph;
+        parent = new Integer[graph.length];
+
+        int start = 0;
+        int end = graph.length - 1;
+        int maxFlow = 0;
+
+        while (bfs(start, end)) {
+            int pathFlow = Integer.MAX_VALUE;
+            int currentNode = end;
+
+            while (currentNode != start) {
+                int prevNode = parent[currentNode];
+                int currentFlow = graph[prevNode][currentNode];
+
+                if (currentFlow > 0 && currentFlow < pathFlow) {
+                    pathFlow = currentFlow;
+                }
+
+                currentNode = prevNode;
+            }
+
+            maxFlow += pathFlow;
+
+            currentNode = end;
+            while (currentNode != start) {
+                int prevNode = parent[currentNode];
+                graph[prevNode][currentNode] -= pathFlow;
+                graph[currentNode][prevNode] += pathFlow;
+
+                currentNode = prevNode;
+            }
+        }
+
+        return maxFlow;
+    }
+
+    private static boolean bfs(int start, int end) {
+        boolean[] visited = new boolean[graph.length];
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+
+            for (int child = 0; child < graph[node].length; child++) {
+                if (graph[node][child] > 0 && !visited[child]) {
+                    queue.offer(child);
+                    parent[child] = node;
+                    visited[child] = true;
+                }
+            }
+        }
+
+        return visited[end];
     }
 }
