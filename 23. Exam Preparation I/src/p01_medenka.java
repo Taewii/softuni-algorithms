8da8cf36ea5400cd4d9c8e8cea6fd131317dc7ff
Taewii <p3rfec7@abv.bg>
2019-04-30 18:00:45 +0300

    1-3 solutions

--- 23. Exam Preparation I/src/p01_medenka.java
+++ 23. Exam Preparation I/src/p01_medenka.java
@@ -1,10 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class p01_medenka {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String medenka = reader.readLine().replaceAll("\\s+", "");
+
+        int nuts = 0;
+        for (int i = 0; i < medenka.length(); i++) {
+            if (medenka.charAt(i) == '1') nuts++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = medenka.indexOf("1");
+        generateCuts(start, 0, nuts, medenka, result);
+    }
+
+    private static void generateCuts(int start, int cuts, int nuts, String medenka, List<Integer> indices) {
+        if (cuts == nuts - 1) {
+            print(medenka, indices);
+        } else {
+            int end = medenka.indexOf("1", start + 1);
+
+            for (int i = start; i < end; i++) {
+                indices.add(i);
+                generateCuts(end, cuts + 1, nuts, medenka, indices);
+                indices.remove(indices.size() - 1);
+            }
+        }
+    }
+
+    private static void print(String medenka, List<Integer> indices) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = medenka.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            sb.append(chars[i]);
+            if (indices.contains(i)) sb.append("|");
+        }
+
+        System.out.println(sb.toString());
     }
 }
--- 23. Exam Preparation I/src/p02_cable_merchant.java
+++ 23. Exam Preparation I/src/p02_cable_merchant.java
@@ -1,10 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class p02_cable_merchant {
+
+    private static int[] prices;
+    private static int[] bestPrices;
+    private static int[] bestCombination;
+    private static int connectorPrice;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] pricesStringArr = reader.readLine().split("\\s+");
+        connectorPrice = Integer.parseInt(reader.readLine());
+
+        prices = new int[pricesStringArr.length + 1];
+        for (int i = 1; i < prices.length; i++) {
+            prices[i] = Integer.parseInt(pricesStringArr[i - 1]);
+        }
+
+        for (int i = 1; i < prices.length; i++) {
+            bestPrices = new int[prices.length];
+            bestCombination = new int[prices.length];
+            System.out.print(cutRod(i) + " ");
+        }
+    }
+
+    private static int cutRod(int n) {
+        int currentBest;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                currentBest = Math.max(bestPrices[i], prices[j] + bestPrices[i - j]);
+                if (currentBest > bestPrices[i]) {
+                    bestPrices[i] = currentBest;
+                    bestCombination[i] = j;
+                }
+            }
+        }
+
+        return Math.max(prices[n], calculateFinalPrice(n));
+    }
+
+    private static int calculateFinalPrice(int n) {
+        int finalPrice = bestPrices[n];
+        int pieces = 0;
+
+        while (n - bestCombination[n] != 0) {
+            pieces++;
+            n = n - bestCombination[n];
+        }
+
+        return finalPrice - 2 * pieces * connectorPrice;
     }
 }
